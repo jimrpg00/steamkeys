7db59cb36da269873a0f4fec57eb08a1b5444d6a
Mark Wong <hello@whizbangapps.xyz>
2021-10-31 22:52:59 +1100

    fixed key restrictions

--- cogs/RequestKey.py
+++ cogs/RequestKey.py
@@ -2,18 +2,23 @@ import logging
 import datetime
 import discord
 from discord.ext import commands
+from firebase_admin import firestore
 
 class RequestKey(commands.Cog):
 
     def __init__(self, client):
         self.client = client
         self.db = self.client.firestoreDb
+        self.keyLimit = 5
 
     @commands.command(brief="Grants a free and valid key")
     async def requestkey(self, ctx):
-        commandArr = ctx.content.split(' ')
-        commandArr.pop(0)
+        commandArr = ctx.message.content.split(' ')
+        commandArr.pop(0) # command - !requestkey
+        commandArr.pop(-1) # game key
         gameTitle = ' '.join(commandArr)
+        print(gameTitle)
+
         # uppercaseGameTitle = gameTitle.upper()
         # availableKey_docs = self.db.collection(uppercaseGameTitle).limit(1).stream()
         availableKey_docs = self.db.collection("game_list").document(gameTitle).collection("keys").limit(1).stream()
@@ -35,22 +40,36 @@ class RequestKey(commands.Cog):
             doc_fields = user_doc.get()
             if "date" in doc_fields.to_dict():
                 prevDateNoTz = doc_fields.to_dict()["date"].replace(tzinfo=None)
-                currDate = datetime.datetime.now().replace(tzinfo=None)
-                dailyLimit = (abs(currDate - prevDateNoTz).days)
-                if dailyLimit >= 1:
+                if "keyLimit" not in doc_fields.to_dict():
                     user_doc.update({
+                        "keyLimit" : 1
+                    })
+                # get updated user profile
+                doc_fields = user_doc.get()
+                currDate = datetime.datetime.now().replace(tzinfo=None)
+                daySince = (abs(currDate - prevDateNoTz).days)
+                userKeyLimit = doc_fields.to_dict()["keyLimit"]
+
+                if daySince >= 1:
+                    user_doc.update({
+                        "keyLimit" : 0,                        
+                })
+                if daySince >= 1 or userKeyLimit < self.keyLimit:
+                    
+                    user_doc.update({
+                        "keyLimit" : firestore.Increment(1), # INCREMENT RATING
                         "date" : datetime.datetime.now().replace(tzinfo=None)
                     })
                 else:
-                    await ctx.author.send(f'You have reached your daily limit of 1 key per day.')
+                    await ctx.author.send(f'You have reached your daily limit of {self.keyLimit} keys per day.')
                     return
             else:
                 user_doc.update({
                     "date" : datetime.datetime.now().replace(tzinfo=None)
                 })
             
-            # a limit of 5 keys per game is allowed, this includes other members of the family
-            if numberOfKeys < 5:
+            # a limit of 5 keys per game
+            if numberOfKeys < self.keyLimit:
                 # add key to user list
                 self.db.collection('user').document(str(ctx.author.id)).update({
                     "name" : ctx.author.name,
@@ -63,9 +82,9 @@ class RequestKey(commands.Cog):
                 })
                 logging.info(f'{gameKey} successfully added to {ctx.author.id}')
 
-                # self.db.collection(uppercaseGameTitle).document(f'{gameKey}').delete()
-                self.db.collection(gameTitle).document(f'{gameKey}').delete()
-                logging.info(f'{gameKey} successfully deleted')
+                # turn back on
+                self.db.collection("game_list").document(gameTitle).collection("keys").document(f'{gameKey}').delete()
+                logging.info(f'{gameKey} successfully deleted and taken')
                 await ctx.channel.send(f'Congrats {ctx.author.name}, check your DM for the key!')
                 await ctx.author.send(f'Your game key for {gameTitle} is {gameKey}. Enjoy!')
             else:
